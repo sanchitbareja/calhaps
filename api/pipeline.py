9af15633c95eb9c3b06b769c784103fc26edbbf1
Sanchit Bareja <sanchitbareja@gmail.com>
2014-01-26 19:34:12 -0800

    gets all fb events

--- api/pipeline.py
+++ api/pipeline.py
@@ -32,26 +32,26 @@ def get_user_profile_pic(backend, details, response, social_user, uid, user, *ar
 def get_user_events(backend, details, response, social_user, uid, user, *args, **kwargs):
     url = None
     if backend.__class__ == FacebookBackend:
-        query = "SELECT name, start_time, timezone, description, pic, location, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid="+str(response['id'])+""
+        query = "SELECT name, start_time, timezone, description, pic, location, eid FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid="+str(response['id'])+")"
         params = urllib.urlencode({'q': query, 'access_token': str(response['access_token'])})
         url = "https://graph.facebook.com/fql?"+params
         print url
         events_data = simplejson.loads(urlopen(url).read())
         print events_data
         for event in events_data['data']:
-            old_event = FacebookEvent.objects.filter(fbId=event['id'])
+            old_event = FacebookEvent.objects.filter(fbId=event['eid'])
             if len(old_event) > 0:
                 old_event = old_event[0]
             else:
                 if 'description' in event.keys():
-                    old_event = FacebookEvent(fbId=event['id'],title=event['name'],description=event['description'],image=event['picture']['data']['url'],startTime=event['start_time'])
+                    old_event = FacebookEvent(fbId=event['eid'],title=event['name'],description=event['description'],image=event['pic'],startTime=event['start_time'])
                 else:
-                    old_event = FacebookEvent(fbId=event['id'],title=event['name'],image=event['picture']['data']['url'],startTime=event['start_time'])
+                    old_event = FacebookEvent(fbId=event['eid'],title=event['name'],image=event['pic'],startTime=event['start_time'])
                 old_event.save()
-            new_event_association = PersonalEvent.objects.filter(user=user, fbEvent=old_event, rsvpStatus=event['rsvp_status'])
-            if len(new_event_association) == 0:
-                new_event_association = PersonalEvent(user=user, fbEvent=old_event, rsvpStatus=event['rsvp_status'])
-                new_event_association.save()
+            # new_event_association = PersonalEvent.objects.filter(user=user, fbEvent=old_event, rsvpStatus=event['rsvp_status'])
+            # if len(new_event_association) == 0:
+            #     new_event_association = PersonalEvent(user=user, fbEvent=old_event, rsvpStatus=event['rsvp_status'])
+            #     new_event_association.save()
         print events_data
 
 # def get_user_network(backend, details, response, social_user, uid, user, *args, **kwargs):
